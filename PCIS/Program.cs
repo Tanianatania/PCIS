5535112367022688ebf36371874b23a6a7a15087
Volodymyr <volodymyr33929@gmail.com>
2018-09-16 11:01:17 +0300

    Continue

--- PCIS/Program.cs
+++ PCIS/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace PCIS
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PCIS
 {
+    interface iShape
+    {
+        
+    }
+    class Circle
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int radius { get; set; }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
