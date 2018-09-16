985a9ea83d8aafddec5c1d308c8a1ba593215455
Volodymyr <volodymyr33929@gmail.com>
2018-09-16 11:13:31 +0300

    Update program.cs

--- PCIS/Program.cs
+++ PCIS/Program.cs
@@ -9,14 +9,33 @@ namespace PCIS
 {
     interface iShape
     {
-        
+        double getSqaure();
+        double getPerimetr();
     }
-    class Circle
+    class Circle:iShape
     {
         public int x { get; set; }
         public int y { get; set; }
         public int radius { get; set; }
 
+        public double getSqaure()
+        {
+            double res = Math.PI * radius * radius;
+            return res;
+        }
+        
+        public double getPerimetr()
+        {
+            double res = 2 * Math.PI * radius;
+            return res;
+        }
+    }
+    class Square:iShape
+    {
+        public double left_upper_angle { get; set; }
+        public double right_bottom_angle { get; set; }
+
+        
     }
     class Program
     {
