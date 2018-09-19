d54de116d729b55f4df35fcf8649c66d94fcc965
tania_gutiy <tatiana.gutiy@gmail.com>
2018-09-19 23:17:05 +0300

    Wrote reading from file to list

---
+++ PCIS.Collections/Data/Task1.cs
@@ -0,0 +1,73 @@
+﻿using PCIS.Models;
+using PCIS.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCIS.Collections.Data
+{
+    internal class Parser
+    {
+        public IShape Parse(string str)
+        {
+            string[] fields = str.Split(' ');
+            string name = fields[0];
+            switch (name)
+            {
+                case "circle":
+                    double x = double.Parse(fields[1]);
+                    double y = double.Parse(fields[2]);
+                    double r = double.Parse(fields[3]);
+                    return new Circle(x, y, r);
+
+
+                case "square":
+                    double left_x = double.Parse(fields[1]);
+                    double left_y = double.Parse(fields[2]);
+                    double right_x = double.Parse(fields[3]);
+                    double right_y = double.Parse(fields[4]);
+                    return new Square(left_x, left_y, right_x, right_y);
+
+                case "triangle":
+                    double first_x = double.Parse(fields[1]);
+                    double first_y = double.Parse(fields[2]);
+                    Point first_point = new Point(first_x, first_y);
+                    double second_x = double.Parse(fields[3]);
+                    double second_y = double.Parse(fields[4]);
+                    Point second_point = new Point(second_x, second_y);
+                    double third_x = double.Parse(fields[5]);
+                    double third_y = double.Parse(fields[6]);
+                    Point third_point = new Point(third_x, third_y);
+                    return new Triangle(first_point, second_point, third_point);
+
+                default:
+                    return null;
+            }
+        }
+    }
+
+    public static class Task1
+    {
+        public static void Run()
+        {
+            List<IShape> list = new List<IShape>();
+            using (var fm = new BasicFileManager(@"C:\Users\Hp\Desktop\PCIS\PCIS.Collections\Data\Data.txt", BasicFileManager.IOType.Reader))
+            {
+                Parser p = new Parser();
+                int count = int.Parse(fm.ReadLine());
+                for (int i = 0; i < count; i++)
+                {
+                    string data = fm.ReadLine();
+                    Console.WriteLine(data);
+                    list.Add(p.Parse(data));
+                }
+
+
+
+            }
+        }
+    }
+}
+
--- PCIS/Program.cs
+++ PCIS/Program.cs
@@ -18,35 +18,8 @@ namespace PCIS
         /// </summary>
         private static void Main()
         {
-            IShape[] shapes =
-            {
-                new Circle(0, 0, 3),
-                new Square(0, 5, 5, 0),
-                new Triangle(new Point(0, 0), new Point(1, 1), new Point(3, 2))
-            };
 
-            foreach (var shape in shapes)
-            {
-                string name;
-                if (shape is Circle)
-                {
-                    name = "Circle";
-                }
-                else if (shape is Square)
-                {
-                    name = "Square";
-                }
-                else if (shape is Triangle)
-                {
-                    name = "Triangle";
-                }
-                else
-                {
-                    name = "Unknown";
-                }
-
-                System.Console.WriteLine($"{name} | Perimeter: {shape.GetPerimeter():00.00}\tSquare: {shape.GetSquare():00.00}");
-            }
+            PCIS.Collections.Data.Task1.Run();
         }
     }
 }
